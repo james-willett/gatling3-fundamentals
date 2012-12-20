0038c0e1bf7a3c21c5226d9beff7acf9eea6a7f8
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2012-12-20 12:18:23 +0100

    add demo Maven project

---
+++ src/test/scala/advanced/AdvancedExampleSimulation.scala
@@ -0,0 +1,17 @@
+package advanced
+
+import com.excilys.ebi.gatling.core.Predef._
+import com.excilys.ebi.gatling.http.Predef._
+import com.excilys.ebi.gatling.jdbc.Predef._
+import com.excilys.ebi.gatling.http.Headers.Names._
+import akka.util.duration._
+import assertions._
+
+class AdvancedExampleSimulation extends Simulation {
+
+	val httpConf = httpConfig.baseURL("http://excilys-bank-web.cloudfoundry.com").disableFollowRedirect
+
+	setUp(
+		SomeScenario.scn.users(10).ramp(10).protocolConfig(httpConf),
+		SomeOtherScenario.otherScn.users(5).ramp(20).delay(30).protocolConfig(httpConf))
+}

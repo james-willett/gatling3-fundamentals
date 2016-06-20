8eaa330ad73c8ae62812e0046d545123e4c01a94
Stephane Landelle <slandelle@gatling.io>
2016-06-20 15:48:10 +0200

    Update sample: such Content-Type is automatically generated

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -9,12 +9,9 @@ class BasicSimulation extends Simulation {
   val httpConf = http
     .baseURL("http://computer-database.gatling.io") // Here is the root for all relative URLs
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
-    .doNotTrackHeader("1")
-    .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
-
-  val headers_10 = Map("Content-Type" -> """application/x-www-form-urlencoded""") // Note the headers specific to a given request
 
   val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
     .exec(http("request_1")
@@ -46,7 +43,6 @@ class BasicSimulation extends Simulation {
     .pause(1)
     .exec(http("request_10") // Here's an example of a POST request
       .post("/computers")
-      .headers(headers_10)
       .formParam("""name""", """Beautiful Computer""") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
       .formParam("""introduced""", """2012-05-30""")
       .formParam("""discontinued""", """""")

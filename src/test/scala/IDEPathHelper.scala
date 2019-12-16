9496c95bbdba5875e79144fdd0ec7a8851ea2c08
Stephane Landelle <slandelle@gatling.io>
2019-12-16 09:29:34 +0100

    Drop PathHelper usage in IDEPathHelper
    Motivation:
    
    io.gatling.commons.util.PathHelper is an internal subject to changes.
    
    Modifications:
    
    Direcly use java.nio.file.{ Path, Paths }
    
    Result:
    
    Don't depend on Gatling internals

--- src/test/scala/IDEPathHelper.scala
+++ src/test/scala/IDEPathHelper.scala
@@ -1,19 +1,14 @@
 import java.nio.file.Paths
 
-import io.gatling.commons.util.PathHelper._
-
 object IDEPathHelper {
 
-	val gatlingConfUrl = Paths.get(getClass.getClassLoader.getResource("gatling.conf").toURI)
-	val projectRootDir = gatlingConfUrl.ancestor(3)
+	private val projectRootDir = Paths.get(getClass.getClassLoader.getResource("gatling.conf").toURI).getParent.getParent.getParent
+	private val mavenTargetDirectory = projectRootDir.resolve("target")
+	private val mavenSrcTestDirectory = projectRootDir.resolve("src").resolve("test")
 
-	val mavenSourcesDirectory = projectRootDir / "src" / "test" / "scala"
-	val mavenResourcesDirectory = projectRootDir / "src" / "test" / "resources"
-	val mavenTargetDirectory = projectRootDir / "target"
-	val mavenBinariesDirectory = mavenTargetDirectory / "test-classes"
-
-	val resourcesDirectory = mavenResourcesDirectory
-	val recorderSimulationsDirectory = mavenSourcesDirectory
-	val resultsDirectory = mavenTargetDirectory / "gatling"
-	val recorderConfigFile = mavenResourcesDirectory / "recorder.conf"
+	val mavenSourcesDirectory = mavenSrcTestDirectory.resolve("scala")
+	val mavenResourcesDirectory = mavenSrcTestDirectory.resolve("resources")
+	val mavenBinariesDirectory = mavenTargetDirectory.resolve("test-classes")
+	val resultsDirectory = mavenTargetDirectory.resolve("gatling")
+	val recorderConfigFile = mavenResourcesDirectory.resolve("recorder.conf")
 }

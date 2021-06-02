45a96b9f45a024626eb38d7a59bd00086d4515c6
James Willett <james.willett58@gmail.com>
2021-06-02 21:19:16 +0100

    Remove proxy from MyFirstTest

--- src/test/scala/MyFirstTest.scala
+++ src/test/scala/MyFirstTest.scala
@@ -6,7 +6,6 @@ class MyFirstTest extends Simulation {
   // 1 Http Conf
   val httpConf = http.baseUrl("http://localhost:8080/app/")
     .header("Accept", "application/json")
-    .proxy(Proxy("localhost", 8888))
 
 
   // 2 Scenario Definition
